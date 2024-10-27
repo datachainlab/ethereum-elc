3676589a9a880ef5372d9d1415a35374ca896365
Jun Kimura <jun.kimura@datachain.jp>
2024-10-27 19:38:50 +0900

    bump ethereum-ibc-rs to v0.0.14
    Signed-off-by: Jun Kimura <jun.kimura@datachain.jp>

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,6 @@
 
 use client::EthereumLightClient;
 use ethereum_ibc::client_state::ETHEREUM_CLIENT_STATE_TYPE_URL;
-use ethereum_ibc::consensus::{bellatrix, deneb};
 use light_client::LightClientRegistry;
 extern crate alloc;
 
@@ -24,34 +23,13 @@ mod internal_prelude {
 }
 use internal_prelude::*;
 
-pub fn register_implementations<
-    const SYNC_COMMITTEE_SIZE: usize,
-    const EXECUTION_PAYLOAD_TREE_DEPTH: usize,
->(
+pub fn register_implementations<const SYNC_COMMITTEE_SIZE: usize>(
     registry: &mut dyn LightClientRegistry,
 ) {
     registry
         .put_light_client(
             ETHEREUM_CLIENT_STATE_TYPE_URL.to_string(),
-            Box::new(EthereumLightClient::<SYNC_COMMITTEE_SIZE, EXECUTION_PAYLOAD_TREE_DEPTH>),
+            Box::new(EthereumLightClient::<SYNC_COMMITTEE_SIZE>),
         )
         .unwrap()
-}
-
-/// Register the light client implementations for the bellatrix and capella testnets
-pub fn register_bellatrix_implementations<const SYNC_COMMITTEE_SIZE: usize>(
-    registry: &mut dyn LightClientRegistry,
-) {
-    register_implementations::<SYNC_COMMITTEE_SIZE, { bellatrix::EXECUTION_PAYLOAD_TREE_DEPTH }>(
-        registry,
-    );
-}
-
-/// Register the light client implementations for the deneb testnet
-pub fn register_deneb_implementations<const SYNC_COMMITTEE_SIZE: usize>(
-    registry: &mut dyn LightClientRegistry,
-) {
-    register_implementations::<SYNC_COMMITTEE_SIZE, { deneb::EXECUTION_PAYLOAD_TREE_DEPTH }>(
-        registry,
-    );
 }
